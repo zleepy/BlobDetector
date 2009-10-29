036f79e2d7bf1bf8c1166e28f6caee2097bd823e
Anders <anders@codeispoetry.com>
2009-10-29 14:23:44 +0100

    Splittade till egna filer.
    Signed-off-by: Anders <anders@codeispoetry.se>

---
+++ BitManipulator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Histogram
+{
+    public static class BitManipulator
+    {
+        public static short ToInt16(byte[] source, int index)
+        {
+            byte shift = (byte)(index & (byte)7);
+            byte value = (byte)(source[index >> 3] & ((byte)128 >> shift));
+            return (short)(value << shift);
+            //return BitConverter.ToInt16(source, index);
+        }
+
+        public static int ToInt32(byte[] source, int index)
+        {
+            byte shift = (byte)(index & (byte)7);
+            byte value = (byte)(source[index >> 3] & ((byte)128 >> shift));
+            return (int)(value << shift);
+            //return BitConverter.ToInt32(source, index);
+        }
+
+        public static bool ToBoolean(byte[] source, int index)
+        {
+            return BitConverter.ToBoolean(source, index);
+        }
+
+        public static void Set(byte[] destination, int index, int value)
+        {
+            Set(destination, index, value != 0);
+        }
+
+        public static void Set(byte[] destination, int index, bool value)
+        {
+            if (value)
+            {
+                //Debug.Write(string.Format("Set: Index={0};", index));
+                //Debug.Write(string.Format("Pos={0};", index >> 3));
+                //Debug.WriteLine(string.Format("Mask={0};", (byte)((byte)128 >> (index & (byte)7))));
+                destination[index >> 3] |= (byte)((byte)128 >> (index & (byte)7));
+            }
+            else
+            {
+                //Debug.Write(string.Format("Unset: Index={0};", index));
+                //Debug.Write(string.Format("Pos={0};", index >> 3));
+                //Debug.WriteLine(string.Format("Mask={0};", ~(byte)((byte)128 >> (byte)(index & 7))));
+                destination[index >> 3] &= (byte)(~(byte)128 >> (byte)(index & 7));
+            }
+        }
+    }
+}
---
+++ Blob.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Histogram
+{
+    public class Blob
+    {
+        /// <summary>
+        /// En rektangel som ramar in blobben.
+        /// </summary>
+        public Rectangle BoundingBox;
+
+        /// <summary>
+        /// Antalet svarta pixlar som ingår i blobben.
+        /// </summary>
+        public uint Area;
+
+        /// <summary>
+        /// En 1bpp mask av vad som hör till den här blobben.
+        /// Även om två blobbar ligger inom varandras regione så syns dom inte i varandras mask.
+        /// </summary>
+        public byte[] Mask;
+
+        public Blob()
+        {
+        }
+
+        public Blob(List<Point> points)
+        {
+            Area = (uint)points.Count;
+            BoundingBox = CreateBoundingBox(points);
+            Mask = CreateMask(BoundingBox, points);
+        }
+
+        private byte[] CreateMask(Rectangle rect, List<Point> points)
+        {
+            byte[] newMask = new byte[(int)Math.Ceiling(rect.Width * rect.Height / 8.0)];
+
+            foreach (var point in points)
+                BitManipulator.Set(newMask, (point.Y - rect.Y) * rect.Width + (point.X - rect.X), true);
+
+            return newMask;
+        }
+
+        private Rectangle CreateBoundingBox(List<Point> points)
+        {
+            int left = points[0].X;
+            int right = points[0].X;
+            int top = points[0].Y;
+            int bottom = points[0].Y;
+
+            for (int i = 1; i < points.Count; i++)
+            {
+                Point p = points[i];
+                if (p.X < left)
+                    left = p.X;
+                if (p.X > right)
+                    right = p.X;
+                if (p.Y < top)
+                    top = p.Y;
+                if (p.Y > bottom)
+                    bottom = p.Y;
+            }
+
+            return new Rectangle(left, top, right - left + 1, bottom - top + 1);
+        }
+    }
+}
