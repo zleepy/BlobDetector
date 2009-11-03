1448b39ba83a775270f5c87cdf7ca85ae54b1c25
Anders <anders@codeispoetry.se>
2009-11-03 08:23:21 +0100

    Rensade lite i BitManipulator.
    Signed-off-by: Anders <anders@codeispoetry.se>

--- BitManipulator.cs
+++ BitManipulator.cs
@@ -9,23 +9,19 @@ namespace Histogram
     {
         public static short ToInt16(byte[] source, int index)
         {
-            byte shift = (byte)(index & (byte)7);
-            byte value = (byte)(source[index >> 3] & ((byte)128 >> shift));
-            return (short)(value << shift);
-            //return BitConverter.ToInt16(source, index);
+            return (short)(ToBoolean(source, index) ? 1 : 0);
         }
 
         public static int ToInt32(byte[] source, int index)
         {
-            byte shift = (byte)(index & (byte)7);
-            byte value = (byte)(source[index >> 3] & ((byte)128 >> shift));
-            return (int)(value << shift);
-            //return BitConverter.ToInt32(source, index);
+            return ToBoolean(source, index) ? 1 : 0;
         }
 
         public static bool ToBoolean(byte[] source, int index)
         {
-            return BitConverter.ToBoolean(source, index);
+            byte shift = (byte)(index & (byte)7);
+            byte value = (byte)(source[index >> 3] & ((byte)128 >> shift));
+            return value != 0;
         }
 
         public static void Set(byte[] destination, int index, int value)
@@ -36,19 +32,9 @@ namespace Histogram
         public static void Set(byte[] destination, int index, bool value)
         {
             if (value)
-            {
-                //Debug.Write(string.Format("Set: Index={0};", index));
-                //Debug.Write(string.Format("Pos={0};", index >> 3));
-                //Debug.WriteLine(string.Format("Mask={0};", (byte)((byte)128 >> (index & (byte)7))));
                 destination[index >> 3] |= (byte)((byte)128 >> (index & (byte)7));
-            }
             else
-            {
-                //Debug.Write(string.Format("Unset: Index={0};", index));
-                //Debug.Write(string.Format("Pos={0};", index >> 3));
-                //Debug.WriteLine(string.Format("Mask={0};", ~(byte)((byte)128 >> (byte)(index & 7))));
                 destination[index >> 3] &= (byte)(~(byte)128 >> (byte)(index & 7));
-            }
         }
     }
 }
